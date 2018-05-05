27f1ae5c313c3b21495b5a36d7cf3c911222d914
github username <github email address>
2018-05-05 18:28:01 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,5 @@
 class Song
  attr_accessor :name, :artist, :genre
-
  # needs .count   .atrist   .genres
  # .number of songs for each genre
  # .number of songs for each artist
@@ -8,8 +7,7 @@ class Song
  @@count = 0
  @@genres = []
  @@artists = []
- @@genre_count = Hash.new(0)
- @@artist_count = Hash.new(0)
+
 
  def initialize(name, artist, genre)
    @name = name
@@ -33,15 +31,22 @@ class Song
    @@artists.uniq
  end
 
- def self.genre_count
-   @@genres.each {|genre| @@genre_count[genre] += 1}
-   return @@genre_count
- end
+#  def self.genre_count
+#   @@genres.each { |genre| @@genre_count[genre] += 1 }
+#   @@genre_count
+# end
+#
+# def self.artist_count
+#   @@artists.each { |artist| @@artist_count[artist] += 1 }
+#   @@artist_count
+# end
+    def self.genre_count
+        @@genres.each_with_object(Hash.new(0)) { |genre, result| result[genre] += 1 }
+    end
 
- def self.artist_count
-   @@artists.each {|artist| @@artist_count[artist] += 1}
-   return @@artist_count
- end
+    def self.artist_count
+        @@artists.each_with_object(Hash.new(0)) { |artist, result| result[artist] += 1 }
+    end
 
 
 end
